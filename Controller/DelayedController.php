49a68ac53ba1765ff2ba2fd8bd337c132980f003
Irvin <irvin.thomas@gsoi.fr>
2018-04-05 12:03:18 +0200

    Add delayed jobs view

---
+++ Controller/DelayedController.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the AllProgrammic ResqueBunde package.
+ *
+ * (c) AllProgrammic SAS <contact@allprogrammic.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AllProgrammic\Bundle\ResqueBundle\Controller;
+
+use AllProgrammic\Bundle\ResqueBundle\Pagination\Paginator;
+use AllProgrammic\Component\Resque\Worker;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DelayedController extends Controller
+{
+    /**
+     * Index action
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        $page = $request->query->get('page', 1);
+
+        // Create new paginator
+        $pager = new Paginator($this->get('resque')->getDelayed());
+        $jobs  = $pager
+            ->setMaxPerPage(15)
+            ->setCurrentPage($page)
+            ->getCurrentPageResults();
+
+        return $this->render('AllProgrammicResqueBundle:delayed:index.html.twig', [
+            'jobs'  => $jobs,
+            'pager' => $pager
+        ]);
+    }
+}
