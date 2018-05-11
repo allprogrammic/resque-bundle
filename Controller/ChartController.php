e61560ed691158d47d471df303a5a5f0aa84c1e6
IrvinT <irvinthomas@outlook.fr>
2018-05-11 10:41:24 +0200

    Add graph (#13)
    * Add graph for processed and failure count

---
+++ Controller/ChartController.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of the AllProgrammic ResqueBunde package.
+ *
+ * (c) AllProgrammic SAS <contact@allprogrammic.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AllProgrammic\Bundle\ResqueBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ChartController extends Controller
+{
+    /**
+     * Index action
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('AllProgrammicResqueBundle:chart:default.html.twig');
+    }
+
+    /**
+     * Failures action
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function failuresAction()
+    {
+        $data = $this->get('resque.charts')->getFailure()->peek(0, 0);
+        $data = $this->getData($data);
+
+        return $this->render('AllProgrammicResqueBundle:chart:failures.html.twig', [
+            'data' => json_encode($data),
+        ]);
+    }
+
+    /**
+     * Processed action
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function processedAction()
+    {
+        $data = $this->get('resque.charts')->getProcess()->peek(0, 0);
+        $data = $this->getData($data);
+
+        return $this->render('AllProgrammicResqueBundle:chart:processed.html.twig', [
+            'data' => json_encode($data),
+        ]);
+    }
+
+    /**
+     * @param $data
+     * @param int $interval
+     *
+     * @return array
+     */
+    public function getData($data, $interval = 7, $format = 'm-d')
+    {
+        $diff = strtotime('now');
+        $result = [];
+
+        for ($i = 0; $i < $interval; $i++) {
+            if ($i !== 0) {
+                $diff = strtotime(sprintf('-%s days', $i));
+            }
+
+            $result[date($format, $diff)] = 0;
+        }
+
+        foreach ($data as $key => $value) {
+            if (!isset($value['date'])) {
+                continue;
+            }
+
+            $date = date('Y-m-d', $value['date']);
+
+            if (strtotime($date) <
+                strtotime(sprintf('-%s days', $interval))) {
+                break;
+            }
+
+            $count = 1;
+
+            if (isset($value['date'])) {
+                $count = $value['count'];
+            }
+
+            $result[date($format, strtotime($date))] += $count;
+        }
+
+        return array_reverse($result);
+    }
+}
--- Resources/public/scripts/application.js
+++ Resources/public/scripts/application.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     asyncAction();
     toggleAction();
+    chartsAction();
 });
 
 var toggleAction = function() {
@@ -20,6 +21,45 @@ var asyncAction = function() {
         setInterval(function(){ reloadContent(el); }, el.getAttribute('async-interval'));
     });
 };
+
+var chartsAction = function() {
+    [].forEach.call(document.querySelectorAll('div[data-charts]'), function(el) {
+        var data   = JSON.parse(el.getAttribute('data-charts'));
+        var div    = document.createElement('div');
+        var canvas = document.createElement('canvas');
+
+        div.classList.add('chart-container');
+        div.appendChild(canvas);
+        el.innerHTML = '';
+        el.appendChild(div);
+
+        new Chart(canvas.getContext('2d'), {
+            type: 'line',
+            data: {
+                labels: Object.keys(data),
+                datasets: [{
+                    label: el.getAttribute('data-label'),
+                    borderColor: 'rgb(206, 18, 18)',
+                    backgroundColor: 'rgb(206, 18, 18)',
+                    data: Object.values(data),
+                    fill: false,
+                }]
+            },
+            options: {
+                legend: false,
+                responsive: true,
+                title: {
+                    display: true,
+                    text: el.getAttribute('data-title'),
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+            }
+        });
+    });
+}
 
 /**
  * Reload content
