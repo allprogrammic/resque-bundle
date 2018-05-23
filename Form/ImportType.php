670529aadd6ba9509b99f6b888a1e5daec5b6900
IrvinT <irvinthomas@outlook.fr>
2018-05-23 15:20:33 +0200

    Add import/export for recurring jobs (#17)

---
+++ Form/ImportType.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the AllProgrammic ResqueBunde package.
+ *
+ * (c) AllProgrammic SAS <contact@allprogrammic.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AllProgrammic\Bundle\ResqueBundle\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType;
+
+class ImportType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('file', FileType::class, [
+                'label' => 'Ficher YAML à importer'
+            ]);
+    }
+}
