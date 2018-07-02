ac3e4767c8de5c08ea59d8ba85b31e8059dd33a4
Irvin <irvin.thomas@gsoi.fr>
2018-07-02 14:05:29 +0200

    Add action to edit failed job

---
+++ Form/FailedType.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the AllProgrammic ResqueBunde package.
+ *
+ * (c) AllProgrammic SAS <contact@allprogrammic.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AllProgrammic\Bundle\ResqueBundle\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType;
+
+class FailedType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('args', TextareaType::class, [
+                'label' => 'Parameters'
+            ])
+            ->add('retry', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('clear', CheckboxType::class, [
+                'required' => false,
+            ]);
+    }
+}
